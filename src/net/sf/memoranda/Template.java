e03ba062226f96014373632661e3c8be5f98d6e4
Tommy <temurph1@asu.edu>
2016-01-30 00:04:50 -0700

    Added Template class with instructions of what needs to be implemented

---
+++ src/net/sf/memoranda/Template.java
@@ -0,0 +1,11 @@
+package net.sf.memoranda;
+
+public class Template {
+
+	// TO-DO: 
+	// 1) Create linked list with tasks/subtasks
+	// 2) Instantiate new Template whenever a user selects "Done" (or Save?) in Template Wizard
+	// 3) Populate user tasks by iterating over template list and then adding to task list
+	// 4) Implement Save feature
+	
+}
