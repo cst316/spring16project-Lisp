b590493b1bac91419c2088b3611be1aa0ba1305b
Eric Dressler <ehicdressler@gmail.com>
2016-02-11 03:08:10 -0700

    Report generator is done, but still needs to be hooked up to the UI. Then needs to be tested. That will be done between 9am and 11am.

--- src/net/sf/memoranda/Report.java
+++ src/net/sf/memoranda/Report.java
@@ -48,7 +48,7 @@ public class Report {
 		//Returns a String that contains the report in HTML
 	public String toHTML {
 		//Doctype and Head
-		String HTML = "<!DOCTYPE html>";
+		String HTML = "<!DOCTYPE html><html>";
 		HTML += "<head><title>Project Report</title></head>";
 		//Body
 		HTML += "<body>";
@@ -58,5 +58,54 @@ public class Report {
 		HTML += "<p>End Date: "+projectEndDate+"</p>";
 		HTML += "<p>Status: "+projectStatus+"</p>";
 		HTML += "<br>";
+		//Iterate through tasklist and add data about each task to the HTML String
+		Task[] taskArr = taskList.getAllSubTasks(null).toArray(taskArr);
+		for (int i=0; i<taskArr.length; ++i) {
+			HTML = getAllSubTasks(HTML, taskArr[i]);
+		}
+		//End of body and HTML
+		HTML +="</body></html>";
+	}
+	
+	private String taskToHTML (String HTML, Task task) {
+		HTML += "<p>Task: "+task.getText()+"</p>";
+		HTML += "<p>Start Date: "+task.getStartDate().toString+"</p>";
+		if(task.getEndDate() == null) {
+			HTML += "<p>End Date: None</p>";
+		} else {
+			HTML += "<p>End Date: "+task.getEndDate().toString+"</p>";
+		}
+		
+		int status = task.getStatus();
+		if(status == 0){
+			HTML += "<p>Status: Scheduled</p>";
+		} else if (status == 1) {
+			HTML += "<p>Status: Active</p>";
+		} else if (status == 2) {
+			HTML += "<p>Status: Completed</p>";
+		} else if (status == 4) {
+			HTML += "<p>Status: Frozen</p>";
+		} else if (status == 5) {
+			HTML += "<p>Status: Failed</p>";
+		}else if (status == 6) {
+			HTML += "<p>Status: Locked</p>";
+		}else {
+			HTML += "<p>Status: Deadline</p>";
+		}
+		
+		int priority = task.getPriority();
+		if(status == 0){
+			HTML += "<p>Priority: Lowest</p>";
+		} else if (status == 1) {
+			HTML += "<p>Priority: Low</p>";
+		} else if (status == 2) {
+			HTML += "<p>Priority: Normal</p>";
+		} else if (status == 3) {
+			HTML += "<p>Priority: High</p>";
+		}else {
+			HTML += "<p>Priority: Highest</p>";
+		}
+		HTML += "<p>Progress: "+task.getProgress()+"</p>";
+		return HTML;
 	}
 }
