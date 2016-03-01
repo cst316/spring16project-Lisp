3c23d962529a6248e35170c51e6736e034c611de
Jason Rice <jarice6@asu.edu>
2016-02-29 20:19:22 -0700

    Added a barebone test file

---
+++ src/net/sf/memoranda/tests/TemplateTest.java
@@ -0,0 +1,37 @@
+package net.sf.memoranda.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import net.sf.memoranda.Template;
+
+public class TemplateTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		Template tmp = new Template("tmp");
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
