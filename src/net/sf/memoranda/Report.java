70c7e42bb2bec496ffb98e1e185903632ba36916
Eric Dressler <ehicdressler@asu.edu>
2016-03-02 13:54:19 -0700

    Fixed bug where high priority is deiplayed for all tasks

--- src/net/sf/memoranda/Report.java
+++ src/net/sf/memoranda/Report.java
@@ -17,7 +17,8 @@ import net.sf.memoranda.date.CurrentDate;
 import java.util.Collection;
 import java.util.Iterator;
 
-//Wrapper class for the TaskListImpl to generate and export an HTML report of a Project and it's Tasks
+//Wrapper class for the TaskListImpl to generate 
+//and export an HTML report of a Project and it's Tasks
 public class Report {
 	
 	private String projectTitle;
@@ -100,7 +101,7 @@ public class Report {
 		}
 		
 		int priority = task.getPriority();
-		if(status == 0){
+		if(priority == 0){
 			s += "Priority: Lowest\n";
 		} else if (status == 1) {
 			s += "Priority: Low\n";
@@ -119,69 +120,69 @@ public class Report {
 	public String toHTML(String id) {
 		Task current = null;
 		//Doctype and Head
-		String HTML = "<!DOCTYPE html><html>";
-		HTML += "<head><title>Project Report</title></head>";
+		String html = "<!DOCTYPE html><html>";
+		html += "<head><title>Project Report</title></head>";
 		//Body
-		HTML += "<body>";
+		html += "<body>";
 		//Project info
-		HTML += "<h2>"+projectTitle+"</h2>";
-		HTML += "<p>Start Date: "+projectStartDate+"</p>";
-		HTML += "<p>End Date: "+projectEndDate+"</p>";
-		HTML += "<p>Status: "+projectStatus+"</p>";
-		HTML += "<br>";
+		html += "<h2>"+projectTitle+"</h2>";
+		html += "<p>Start Date: "+projectStartDate+"</p>";
+		html += "<p>End Date: "+projectEndDate+"</p>";
+		html += "<p>Status: "+projectStatus+"</p>";
+		html += "<br>";
 		//Iterate through tasklist and add data about each task to the HTML String
 		for (Iterator iterator = taskList.getAllSubTasks(null).iterator(); iterator.hasNext();) {
 			current = (Task)iterator.next();
-			HTML = taskToHTML(HTML, current);    
-			HTML += "<br>";
+			html = taskToHTML(html, current);    
+			html += "<br>";
 			if(current.hasSubTasks(current.getID())) {
-				HTML += toString(current.getID());
+				html += toString(current.getID());
 			}
 		}
 		//End of body and HTML
-		HTML +="</body></html>";
-		return HTML;
+		html +="</body></html>";
+		return html;
 	}
 	
-	private String taskToHTML (String HTML, Task task) {
-		HTML += "<p>Task: "+task.getText()+"</p>";
-		HTML += "<p>Start Date: "+task.getStartDate().toString()+"</p>";
+	private String taskToHTML (String html, Task task) {
+		html += "<p>Task: "+task.getText()+"</p>";
+		html += "<p>Start Date: "+task.getStartDate().toString()+"</p>";
 		if(task.getEndDate() == null) {
-			HTML += "<p>End Date: None</p>";
+			html += "<p>End Date: None</p>";
 		} else {
-			HTML += "<p>End Date: "+task.getEndDate().toString()+"</p>";
+			html += "<p>End Date: "+task.getEndDate().toString()+"</p>";
 		}
 		
 		int status = task.getStatus(CurrentDate.get());
 		if(status == 0){
-			HTML += "<p>Status: Scheduled</p>";
+			html += "<p>Status: Scheduled</p>";
 		} else if (status == 1) {
-			HTML += "<p>Status: Active</p>";
+			html += "<p>Status: Active</p>";
 		} else if (status == 2) {
-			HTML += "<p>Status: Completed</p>";
+			html += "<p>Status: Completed</p>";
 		} else if (status == 4) {
-			HTML += "<p>Status: Frozen</p>";
+			html += "<p>Status: Frozen</p>";
 		} else if (status == 5) {
-			HTML += "<p>Status: Failed</p>";
+			html += "<p>Status: Failed</p>";
 		}else if (status == 6) {
-			HTML += "<p>Status: Locked</p>";
+			html += "<p>Status: Locked</p>";
 		}else {
-			HTML += "<p>Status: Deadline</p>";
+			html += "<p>Status: Deadline</p>";
 		}
 		
 		int priority = task.getPriority();
-		if(status == 0){
-			HTML += "<p>Priority: Lowest</p>";
-		} else if (status == 1) {
-			HTML += "<p>Priority: Low</p>";
-		} else if (status == 2) {
-			HTML += "<p>Priority: Normal</p>";
-		} else if (status == 3) {
-			HTML += "<p>Priority: High</p>";
+		if(priority == 0){
+			html += "<p>Priority: Lowest</p>";
+		} else if (priority == 1) {
+			html += "<p>Priority: Low</p>";
+		} else if (priority == 2) {
+			html += "<p>Priority: Normal</p>";
+		} else if (priority == 3) {
+			html += "<p>Priority: High</p>";
 		}else {
-			HTML += "<p>Priority: Highest</p>";
+			html += "<p>Priority: Highest</p>";
 		}
-		HTML += "<p>Progress: "+task.getProgress()+"</p>";
-		return HTML;
+		html += "<p>Progress: "+task.getProgress()+"</p>";
+		return html;
 	}
 }
